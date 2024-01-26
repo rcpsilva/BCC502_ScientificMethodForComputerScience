9b27ca8cd57dc3bd18ce9d76be98f056effab4ad
rcpsilva <rcpsilva@gmail.com>
2024-01-25 22:06:33 -0300

    Create cronograma.mk

---
+++ 2024-1/cronograma.mk
@@ -0,0 +1,43 @@
+| Semana      | Conteúdo                                          	|
+| ----------- | ---------------------------------------------------	|
+| 26/03/2024  | Computação e Classificação das Ciências				|
+| 02/04/2024  | Método Científico                  					|
+| 09/04/2024  | Método Científico								 	|
+| 16/04/2024  | Tipos de pesquisa								 	|
+| 23/04/2024  | Maturidade em pesquisa				             	|
+| 30/04/2024  | Elementos da pesquisa (I)							|
+| 07/05/2024  | Elementos da pesquisa (II)                     		|
+| 14/05/2024  | Revisão bibliográfica 		             			|
+| 21/05/2024  | Questões de Pesquisa					 			|
+| 28/05/2024  | Análise crítica de Propostas de Pesquisa (Início do desenvolvimento da proposta de pesquisa)          	|
+| 04/06/2024  | Ética acadêmica (Desenvolvimento da proposta de pesquisa )           							|
+| 11/06/2024  | Desenvolvimento da proposta de pesquisa             |
+| 18/06/2024  | Desenvolvimento da proposta de pesquisa             |
+| 25/06/2024  | Apresentação dos projetos               			|
+| 02/07/2024  | Apresentação dos projetos               			|
+| 09/07/2024  | Apresentação dos projetos               			|
+| 23/07/2024  | Exame Especial                            			|
+
+Metodologia:
+
+Aulas expositivas sobre o conteúdo programático.
+
+Estudos dirigidos: Atividades individuais que podem ser entrevistas ou testes feitos durante as aulas.
+
+Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Produção de textos científicos.
+
+Apresentações dos trabalhos desenvolvidos pelos alunos.
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Atividades avaliativas:
+
+Estudos dirigidos (E) no valor de 10 pontos.
+
+Proposta de projeto de pesquisa (P), no valor de 10 pontos.
+
+Apresentação da proposta (A), no valor de 10 pontos
+
+Nota final = 0.3 * E + 0.35 * P + 0.35 * A
