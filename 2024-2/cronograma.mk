ddf508aead12e124d166ea0eb5152b317f39b05c
rcpsilva <rcpsilva@gmail.com>
2024-10-15 18:31:27 -0300

    Create cronograma.mk

---
+++ 2024-2/cronograma.mk
@@ -0,0 +1,42 @@
+ Semana     & Conteúdo                                          	
+ ---------- &---------------------------------------------------	
+ 1 			& Tipos de pesquisa em Ciência da Computação                  					
+ 2 			& Estudo Dirigido (Participação da Brazilian Conference on Intelligent Systems (BRACIS 2024)
+ 3 			& Elementos da pesquisa (I) 
+ 4 			& Elementos da pesquisa (II)
+ 5 			& Elementos da pesquisa (III) 
+ 6 			& Análise crítica de Propostas de Pesquisa    
+ 7 			& Definição do Problema e Contextualização (Entrega 21/01)
+ 8 			& Definição do Problema e Contextualização 
+ 9 			& Definição dos Objetivos (Entrega 04/02)
+ 10 		& Definição dos Objetivos
+ 11 		& Definição e Justificativa da Hipótese de Trabalho (Entrega 18/02)
+ 12 		& Definição e Justificativa da Hipótese de Trabalho
+ 13			& Carnaval
+ 14 		& Definição do procedimento metodológico (Entrega 11/03)
+ 15 		& Definição do procedimento metodológico 
+ 16 		& Versão Final do Projeto de Pesquisa (Entrega 25/03)
+ 17 		& Apreciação dos projetos de Pesquisa
+ 08/04/2025 & Exame Especial                           			
+
+Metodologia:
+
+- Aulas expositivas sobre o conteúdo programático.
+
+- Estudos dirigidos: Atividades individuais que podem ser entrevistas ou testes feitos durante as aulas.
+
+- Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+- Produção de projeto de pesquisa: Ao final do curso, cada aluno deverá desenvolver um projeto de pesquisa contendo: (i) Contextualização e definição do problema de pesquisa; (ii) Objetivos; (iii) Definição e Justificativa da hipótese; e (iv) Definição do procedimento metodológico.
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Atividades avaliativas:
+
+- Estudos dirigido (E) no valor de 10 pontos.
+
+- Parciais do projeto de pesquisa (P1, P2, P3, P4) no valor de 10 pontos cada
+
+- Versão final do projeto de pesquisa (F) no valor de 10 pontos 
+
+Nota final = 0.05 * E + 0.05 * P1 + 0.1 * P2 + 0.1 * P3 + 0.2 * P4 + 0.5 * F
