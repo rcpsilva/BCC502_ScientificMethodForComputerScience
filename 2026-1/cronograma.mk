6915076bf4d4e290f1a182fc475d84d12b52a0c0
rcpsilva <rcpsilva@gmail.com>
2026-02-18 21:12:40 -0300

    Create cronograma.mk

---
+++ 2026-1/cronograma.mk
@@ -0,0 +1,42 @@
+ Semana     & Conteúdo                                          	
+ ---------- &---------------------------------------------------	
+ 1 			& (Feriado) Sexta-Feira Santa
+ 2			& Introdução à filosofia da ciência: Indutivismo                   					
+ 3 			& Introdução à filosofia da ciência: O Problema da Indução
+ 4 			& Introdução à filosofia da ciência: Falsificacionismo
+ 5 			& (Feriado) Dia do Trabalhador
+ 6			& Introdução à filosofia da ciência: Teorias como estruturas
+ 7 			& Introdução à filosofia da ciência: Racionalismo, relativismo e objetivismo
+ 8 			& Introdução à filosofia da ciência: Realismo, Instrumentalismo e Verdade  
+ 9 			& Prova (29/05/2026)
+ 10 		& (Feriado) Corpus Christi 
+ 11         & Tipos de Pesquisa em Ciência da Computação 
+ 12			& Contextualização, Definição dos Objetivos e da Justificativa da Hipótese de Trabalho 
+ 13 		& Apresentação da Entrega Parcial 1 (P1) (26/06/2026)
+ 14 		& Definição do procedimento metodológico
+ 15 		& Apresentação da Entrega Parcial 2 (P2) (17/07/2026)
+ 16			& Versão Final do Projeto de Pesquisa (20/07/2026)
+ 17 		& Apreciação dos Projetos de Pesquisa 
+ 18 		& Exame Especial (31/07/2026)                        			
+
+Metodologia:
+
+- Aulas expositivas sobre o conteúdo programático.
+
+- Prova: Teste individual feito durante a aula.
+
+- Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+- Produção de projeto de pesquisa: Em grupo, ao final do curso, os alunos devem desenvolver um projeto de pesquisa contendo: (i) Contextualização e definição do problema de pesquisa; (ii) Objetivos; (iii) Definição e Justificativa da hipótese; e (iv) Definição do procedimento metodológico.
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Atividades avaliativas:
+
+- Prova (P) no valor de 10 pontos.
+
+- Parciais do projeto de pesquisa (P1, P2) no valor de 10 pontos cada
+
+- Versão final do projeto de pesquisa (F) no valor de 10 pontos 
+
+Nota final = 0.3 * P + 0.1 * P1 + 0.1 * P2 + 0.5 * F
